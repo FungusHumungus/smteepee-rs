fbff0dd52ec6f1cdfdb29f946a143d67763aca82
Stephen Wakely <fungus.humungus@gmail.com>
2020-08-11 18:11:55 +0100

    Spawn a new task for each connection.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, net, path, time};
-use tokio_util::codec::{Framed, LinesCodec};
 use tokio::{net::TcpListener, stream::StreamExt};
+use tokio_util::codec::{Framed, LinesCodec};
 
 #[macro_use]
 extern crate lazy_static;
@@ -34,41 +34,47 @@ fn load_settings() -> Result<settings::Settings, Box<dyn std::error::Error>> {
     }
 }
 
-/// Load settings from a toml file if it has beet specified.
-/// Else use the defaults.
+// Load settings from a toml file if it has been specified.
+// Else use the defaults.
 lazy_static! {
-    static ref SETTINGS:settings::Settings = load_settings().unwrap();
+    static ref SETTINGS: settings::Settings = load_settings().unwrap();
 }
-
 
 /// The main function.
 /// Sets up the socket and handles incoming requests.
 #[tokio::main]
 async fn main() {
-
     // Setup the socket.
     let addr = get_listen_address(SETTINGS.protocol, SETTINGS.port);
     let mut listener = TcpListener::bind(&addr).await.unwrap();
 
+    println!("Listenig on port {} v{}", SETTINGS.port, SETTINGS.protocol);
+
     while let Some(stream) = listener.next().await {
-        match stream {
-            Ok(stream) => {
-                let framed = Framed::new(stream, LinesCodec::new());
-                let message = smtp::converse(framed, &SETTINGS).await.unwrap();
-                let now = time::SystemTime::now();
-                match now.duration_since(time::SystemTime::UNIX_EPOCH) {
-                    Ok(n) => {
-                        message.save_to_file(format!("./received/{}.eml", n.as_millis())).await.unwrap();
-                    },
-                    Err(_) => {
-                        // TODO Insert some kind of McFly joke...
-                        eprintln!("We have gone back in time!");
-                    },
+        tokio::spawn(async move {
+            match stream {
+                Ok(stream) => {
+                    println!("New connection!");
+                    let framed = Framed::new(stream, LinesCodec::new());
+                    let message = smtp::converse(framed, &SETTINGS).await.unwrap();
+                    let now = time::SystemTime::now();
+                    match now.duration_since(time::SystemTime::UNIX_EPOCH) {
+                        Ok(n) => {
+                            message
+                                .save_to_file(format!("./received/{}.eml", n.as_millis()))
+                                .await
+                                .unwrap();
+                        }
+                        Err(_) => {
+                            // TODO Insert some kind of McFly joke...
+                            eprintln!("We have gone back in time!");
+                        }
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Connection failed {}", e);
                 }
             }
-            Err(e) => {
-                eprintln!("Connection failed {}", e);
-            }
-        }
+        });
     }
 }
